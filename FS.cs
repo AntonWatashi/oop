f48a6b9e8c94293411645b04b7c80e4e2678ac34
AntonWatashi <34251825+AntonWatashi@users.noreply.github.com>
2018-05-29 22:57:01 +0300

    Update FS.cs

--- FS.cs
+++ FS.cs
@@ -3,15 +3,67 @@ namespace intertion
 {
     public class FS
     {
-        public static int[,] ParseLevel(string[] lines)
+        const int VWALL = 1;
+        const int HWALL = 7;
+        const int TLC = 4;
+        const int BLC = 8;
+        const int BRC = 6;
+        const int TRC = 5;
+        const int SHIELD = 10;
+        const int ARROW = 11;
+        const int TP = 12;
+        const int TRAP = 3;
+        const int AWARD = 2;
+        const int GAP = 0;
+        public static Cell[,] ParseLevel(string[] lines)
         {
-            int[,] wall = new int[lines.Length, lines[0].Split(',').Length];
+            
+            Cell[,] wall = new Cell[lines.Length, lines[0].Split(',').Length];
             for (int i = 0; i < lines.Length; i++)
             {
                 string[] temp = lines[i].Split(',');
                 for (int j = 0; j < temp.Length; j++)
 
-                    wall[i, j] = Int32.Parse(temp[j]);
+                    switch (Int32.Parse(temp[j]))
+                    {
+                           
+                        case VWALL:
+                            wall[i, j] = Cell.VWALL;
+                            break;
+                        case TRAP:
+                            wall[i, j] = Cell.TRAP;
+                            break;
+                        case AWARD:
+                            wall[i, j] = Cell.AWARD;
+                            break;
+                        case HWALL:
+                            wall[i, j] = Cell.HWALL;
+                            break;
+                        case TLC:
+                            wall[i, j] = Cell.TLC;
+                            break;
+                        case BLC:
+                            wall[i, j] = Cell.BLC;
+                            break;
+                        case BRC:
+                            wall[i, j] = Cell.BRC;
+                            break;
+                        case TRC:
+                            wall[i, j] = Cell.TRC;
+                            break;
+                        case ARROW:
+                            wall[i, j] = Cell.ARROW;
+                            break;
+                        case TP:
+                            wall[i, j] = Cell.TP;
+                            break;
+                        case SHIELD:
+                            wall[i, j] = Cell.SHIELD;
+                            break;
+                        case  GAP:
+                            wall[i, j] = Cell.GAP;
+                            break;
+                    }
             }
             return wall;
         }
