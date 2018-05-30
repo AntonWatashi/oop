6bec6e69251e894077867ac33d42ffa2ceb48855
AntonWatashi <34251825+AntonWatashi@users.noreply.github.com>
2018-05-30 04:53:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,47 +9,66 @@ namespace intertion
 {
     class MainClass
     {
+        
         public static void Main(string[] args)
         {
 
             //design
             Console.CursorVisible = false;
-            Console.CursorVisible = false;
-            char trap = '♞';
             char player = '♛';
-            char award = '♟';
-            char rest = '◴';
-            var level = FS.ParseLevel(File.ReadAllLines("map1.txt"));
+             
+            var hero = new Player(5, 5, player);
+            Game(player, hero);
+        }
+
+        static void Game (char player, Player hero)
+        {
+            hero.Reset();
+            string lvl = "map" + hero.counter + ".txt";
+            var level = FS.ParseLevel(File.ReadAllLines(lvl));
 
             var map = new Map(); // char award, char trap
+
+           
+            var enemy = new Enemy(6, 6, '@');
+
             //design
-                var design = new Design();
-                design.Menu(25, 6);
+            var design = new Design();
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (hero.counter == 1)
+            {
+                
                 design.Select(25, 8, '☞');
+                design.getName(hero);
                 design.Loading(28, 10);
 
-                
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-
+            }
+            else
+            {
+                Console.SetCursorPosition(28, 10);
+                Console.Write("Pess any key to continue...");
+                Console.ReadKey(true);
+                design.Loading(28, 10);
+            }
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             //player creation
 
-            var hero = new Player(5, 5, player);
-                
+
             //Hud creation ( hp and points)
             Design.hud(hero);
 
             // update object
-            var upd = new Update(hero, trap, player, award, rest, level);
+            var upd = new Update(hero, level);
 
             Map.DrawMap(level);
-                //enemy.Draw();
-                hero.Draw();
-                design.Rules(hero, trap, award, rest, player);
+            //enemy.Draw();
+            hero.Draw();
+            design.Rules(hero, Cell.TRAP, Cell.AWARD, Cell.TP, player);
             //Thread sw = new Thread(stopWatch);
             //sw.Start();
 
             //game
-            while (true)
+            while (!hero.win)
             {
 
                 if (Console.KeyAvailable)
@@ -57,11 +76,10 @@ namespace intertion
 
                     var key = Console.ReadKey(true);
                     upd.Start(key);
-
-
-
                 }
             }
-        }
+            hero.counter++;
+            Game(player, hero);
+        } 
     }
 }
